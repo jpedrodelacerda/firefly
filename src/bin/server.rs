27052be199f60fa209c826c2e848433f1e492ff5
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-03-21 21:38:52 -0300

    refactor(server): create chat and server structs, still missing server method to handle clients

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,48 +1,11 @@
 use std::io;
-use std::net::TcpListener;
 
-use firefly::Codec;
-
-fn start_listen(listener: TcpListener) -> io::Result<()> {
-	println!("Starting firefly server on {}", listener.local_addr()?);
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("new client");
-                let codec = Codec::new(stream)?;
-
-                handle_client(codec);
-            }
-            Err(e) => {
-                println!("failed to read:\n{}", e);
-            }
-        }
-    }
-    Ok(())
-}
-
-fn handle_client(mut codec: Codec) {
-    codec.send_message("welcome to firefly!\n").unwrap();
-    loop {
-        let line = codec.read_message();
-        match line {
-            Ok(msg) => println!("{}", msg),
-            Err(e) => {
-                println!("{}", e);
-                break;
-            }
-        }
-    }
-}
-
-fn new(address: String, port: String) -> io::Result<TcpListener> {
-    let conn_string = format!("{}:{}", address, port);
-    return TcpListener::bind(conn_string);
-}
+use firefly::Server;
 
 fn main() -> io::Result<()> {
     let addr = String::from("127.0.0.1");
     let port = String::from("1337");
-    let listener = new(addr, port).unwrap();
-    return start_listen(listener);
+	let conn_string = format!("{}:{}", addr, port);
+	let server = Server::new(conn_string).unwrap();
+	server.start_listen()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::io::Error;
 use std::io::{self, BufRead, Write};
-use std::net::TcpStream;
+use std::net::{TcpListener, TcpStream};
 
 pub struct Codec {
     // stream: TcpStream,
@@ -17,8 +18,8 @@ impl Codec {
 
     pub fn send_message(&mut self, message: &str) -> io::Result<usize> {
         let msg_size = self.writer.write(&message.as_bytes())?;
-		self.writer.flush()?;
-		Ok(msg_size)
+        self.writer.flush()?;
+        Ok(msg_size)
     }
 
     pub fn read_message(&mut self) -> Result<String, Error> {
@@ -32,5 +33,93 @@ impl Codec {
         self.reader.consume(received.len());
         let msg = String::from_utf8(received).unwrap();
         Ok(msg)
+    }
+}
+
+pub struct Server {
+    chats: HashMap<String, Chat>,
+    listener: TcpListener,
+}
+
+impl Server {
+    pub fn new(conn_string: String) -> io::Result<Server> {
+        let listener = TcpListener::bind(conn_string).unwrap();
+        let chats: HashMap<String, Chat> = HashMap::new();
+        Ok(Self {
+            chats,
+            listener: listener,
+        })
+    }
+
+    pub fn create_chat(mut self, key: String, conn1: Codec) -> io::Result<()> {
+        let chat_key = &key.clone();
+        let chat = Chat::new(key, conn1).unwrap();
+        match self.chats.get(chat_key) {
+            Some(k) => Err(Error::new(
+                io::ErrorKind::AlreadyExists,
+                format!("the key {} is already in use", k.key),
+            )),
+            None => {
+                self.chats.insert(chat_key.to_string(), chat);
+                match self.chats.get(chat_key) {
+                    Some(_c) => Ok(()),
+                    None => Err(Error::new(
+                        io::ErrorKind::PermissionDenied,
+                        "failed to create chat",
+                    )),
+                }
+            }
+        }
+    }
+
+    pub fn start_listen(self) -> io::Result<()> {
+        println!("Starting firefly server on {}", self.listener.local_addr()?);
+        for stream in self.listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("new client");
+                    let mut conn = Codec::new(stream)?;
+
+                    conn.send_message("welcome to firefly!\n").unwrap();
+                    loop {
+                        let line = conn.read_message();
+                        match line {
+                            Ok(msg) => println!("{}", msg),
+                            Err(e) => {
+                                println!("{}", e);
+                                break;
+                            }
+                        }
+                    }
+
+                    // self.create_chat(String::from("kkkk"), conn);
+                }
+                Err(e) => {
+                    println!("failed to read:\n{}", e);
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Default)]
+pub struct Chat {
+    pub key: String,
+    conn1: Option<Codec>,
+    conn2: Option<Codec>,
+}
+
+impl Chat {
+    pub fn new(key: String, conn1: Codec) -> io::Result<Self> {
+        Ok(Self {
+            key,
+            conn1: Some(conn1),
+            conn2: None,
+        })
+    }
+
+    pub fn connect_to_chat(mut self, conn2: Codec) {
+        self.conn2 = Some(conn2)
     }
 }
